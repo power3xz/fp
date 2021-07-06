5f0724cdf3b103ae08dc9646f68dbd8aabd68cd1
devj <power3xz@gmail.com>
2021-07-06 23:27:39 +0900

    promise

--- 7.js
+++ 7.js
@@ -11,3 +11,14 @@ const square = (a) => {
 square(10).then(square).then(square).then(console.log);
 
 _.go(square(10), square, square, console.log);
+
+const list = [2, 3, 4];
+new Promise((resolve) => {
+  (function recur(res) {
+    if (list.length == res.length) return resolve(res);
+    square(list[res.length]).then((val) => {
+      res.push(val);
+      recur(res);
+    });
+  })([]);
+}).then(console.log);
